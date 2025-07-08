5858d82a9bc35d4737e4d6db5827e375d59382e1
S SIDHARTH <sidharthpunalur@gmail.com>
2025-07-08 16:51:11 +0530

    Task home added

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  const handleTask = (task: string) => {
+    alert(`You selected ${task}`);
+  };
+
+  return (
+    <div className="app">
+      <div className="content">
+        <h1>Welcome to <span className="highlight">AeroCET</span> Recruitment 2025</h1>
+        <p>Official Aeromodelling Club of College of Engineering, Trivandrum</p>
+        <div className="button-group">
+          <button onClick={() => handleTask('Task 1')}>Task 1</button>
+          <button onClick={() => handleTask('Task 2')}>Task 2</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
